14879f45e1213af4a4ee2cb52cb7a57bf6902a95
Kevin Griffin <kevin@kevgriffin.com>
2023-03-02 15:09:57 -0500

    wip hangfire

---
+++ Course/HangFire/Examples/02_Configuration/END/HangFireTactics.Web/Program.cs
@@ -0,0 +1,23 @@
+using Hangfire;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// configure logging to console
+builder.Logging.AddConsole();
+
+builder.Services.AddHangfire(config =>
+{
+    // use DefaultConnection connection string
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    config.UseSqlServerStorage(connectionString);
+    config.UseColouredConsoleLogProvider();
+});
+
+
+builder.Services.AddHangfireServer();
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
---
+++ Course/HangFire/README.md
@@ -0,0 +1,25 @@
+# Tactics for Building Background Services: HangFire
+
+## Overview - Why Background Services?
+
+## Module: Introduction to HangFire
+
+## Module: Installation
+
+## Module: Configuration
+
+## Module: Setting up the Dashboard
+
+## Module: Anatomy of a Job
+
+## Module: On-demand Jobs
+
+## Module: Scheduled/Delayed Jobs
+
+## Module: Recurring Jobs
+
+## Module: Monitoring Jobs
+
+## Module: Handling Errors
+
+## Module: Continuations
