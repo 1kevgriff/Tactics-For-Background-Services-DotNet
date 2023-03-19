63279d01569cf2c16bc6f000e1265d456afd0c5c
Kevin Griffin <kevin@kevgriffin.com>
2023-03-19 13:52:43 -0400

    wip

---
+++ Course/HangFire/Examples/08_TriggerRecurringJobs/START/HangFireTactics.Web/Program.cs
@@ -0,0 +1,83 @@
+using System.Text.Json;
+using Hangfire;
+using HangFireTactics.Web.Jobs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// configure logging to console
+builder.Logging.AddConsole();
+
+builder.Services.AddHttpClient();
+
+builder.Services.AddHangfire(config =>
+{
+    // use DefaultConnection connection string
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    config.UseSqlServerStorage(connectionString);
+    config.UseColouredConsoleLogProvider();
+});
+
+builder.Services.AddHangfireServer();
+
+var app = builder.Build();
+
+app.MapHangfireDashboard();
+app.MapGet("/", () => "Hello World!");
+
+
+var url = "https://consultwithgriff.com/rss.xml";
+var directory = $"c:\\rss";
+var filename = "consultwithgriff.json";
+var tempPath = Path.Combine(directory, filename);
+
+RecurringJob.AddOrUpdate<WebPuller>("pull-rss-feed",
+                                    p => p.GetRssItemUrlsAsync(url, tempPath),
+                                    "* * * * *");
+
+// app.MapGet("/pull", (IBackgroundJobClient client) =>
+// {
+//     var url = "https://consultwithgriff.com/rss.xml";
+//     var directory = $"c:\\rss";
+//     var filename = "consultwithgriff.json";
+//     var tempPath = Path.Combine(directory, filename);
+
+//     // TODO: background work
+//     client.Enqueue<WebPuller>(p => p.GetRssItemUrlsAsync(url, tempPath));
+// });
+
+app.MapGet("/sync", (IBackgroundJobClient client) =>
+{
+
+    var directory = $"c:\\rss";
+    var filename = "consultwithgriff.json";
+
+    var path = Path.Combine(directory, filename);
+    var json = File.ReadAllText(path);
+    var rssItemUrls = JsonSerializer.Deserialize<List<string>>(json);
+
+    var outputPath = Path.Combine(directory, "output");
+    if (!Directory.Exists(outputPath)) Directory.CreateDirectory(outputPath);
+
+    if (rssItemUrls == null || rssItemUrls.Count == 0) return;
+    var delayInSeconds = 5;
+
+    foreach (var url in rssItemUrls)
+    {
+        var u = new Uri(url);
+        var stub = u.Segments.Last();
+        // trim trailing slash, if any and add .html
+        if (stub.EndsWith("/")) stub = stub.Substring(0, stub.Length - 1);
+        stub += ".html";
+
+        var filePath = Path.Combine(outputPath, stub);
+
+        var dt = DateTimeOffset.UtcNow.AddSeconds(delayInSeconds);
+
+        client.Schedule<WebPuller>(p => p.DownloadFileFromUrl(url, filePath),
+            TimeSpan.FromSeconds(delayInSeconds));
+
+        delayInSeconds += 5;
+    }
+});
+
+app.Run();
--- Course/HangFire/README.md
+++ Course/HangFire/README.md
@@ -10,24 +10,50 @@ Server=localhost,1433;Database=HangFire;User Id=sa;Password=YourStrong!Passw0rd;
 
 ## Overview - Why Background Services?
 
-## Module: Introduction to HangFire
+## Module: Introduction to HangFire 
 
 ## Module: Installation
+done
 
 ## Module: Configuration
+done
 
 ## Module: Setting up the Dashboard
+done
 
 ## Module: Anatomy of a Job
+done
 
 ## Module: On-demand Jobs
+done
+
+## Module: Handling Errors
+done
 
 ## Module: Scheduled/Delayed Jobs
+done
 
 ## Module: Recurring Jobs
 
+## Module: Naming Recurring Jobs
+
+## Module: Triggering Recurring Jobs
+
+## Module: DI vs. Static Methods
+
+
+
 ## Module: Monitoring Jobs
 
-## Module: Handling Errors
-
 ## Module: Continuations
+
+## Module: Job Servers
+
+## Modules: Commonly changed HangFire settings
+* Configuring the Number of Workers: Hangfire allows you to configure the number of worker threads that process jobs. Increasing the number of workers can help to reduce the time taken to process jobs, but it may also increase the load on the system.
+
+* Setting the Queue Processing Timeout: Hangfire allows you to set a timeout value for the processing of a job in a queue. If the job processing time exceeds this value, the job is considered to have failed, and it will be re-queued for processing.
+
+* Enabling Automatic Retry: Hangfire provides an automatic retry mechanism that can be configured to automatically retry failed jobs a certain number of times. This feature can help to improve the reliability of your application by automatically retrying failed jobs.
+
+* Configuring the Dashboard: Hangfire provides a dashboard that displays information about the background jobs, such as the number of jobs processed, failed, or enqueued. You can configure the dashboard to display additional information or customize the dashboard UI to fit your needs.
