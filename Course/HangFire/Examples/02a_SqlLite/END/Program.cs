5a436e54e5358d7663fcf002fcaf0d3154ce5b0b
Kevin Griffin <kevin@kevgriffin.com>
2023-04-22 15:49:37 -0400

    wip

---
+++ Course/HangFire/Examples/02a_SqlLite/END/Program.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using HangfireTactics.Web.Data;
+using Hangfire;
+using Hangfire.Storage.SQLite;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddRazorPages();
+
+// TODO: configure SQLite
+builder.Services.AddHangfire(configure => {
+    configure.UseSQLiteStorage();
+    
+    configure.UseColouredConsoleLogProvider();
+});
+builder.Services.AddHangfireServer();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+app.MapHangfireDashboard();
+
+app.MapRazorPages();
+
+app.Run();
